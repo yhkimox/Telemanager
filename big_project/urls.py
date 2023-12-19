2cd6817e95096ab66c752aa115a45026fa87313e
minsung <sbr04119@naver.com>
2023-12-19 17:21:31 +0900

    add, delete, edit client

--- big_project/urls.py
+++ big_project/urls.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 def index(request):
     return render(request, 'home.html')
 
--- client/models.py
+++ client/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Client(models.Model):
+	name = models.CharField(max_length=70)
+	location = models.CharField(max_length=140, null=True, blank=True)
+	number = models.CharField(max_length=80, null=True, blank=True)
