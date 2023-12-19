f59dd1989fac9940110bf97affcf9563c0f7f68c
hyeon8922 <hyeon8922@naver.com>
2023-12-19 16:38:38 +0900

    'hj_ui_temp'

--- big_project/urls.py
+++ big_project/urls.py
@@ -1,3 +1,5 @@
+# big_project/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
@@ -5,12 +7,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 def index(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path('', index),
+    path("admin/", admin.site.urls),
     path('account/', include('account.urls')),
     path('chatbot/', include('chatbot.urls')),
     path('client/', include('client.urls')),
+    path('index.html', index, name='index'),  # /index.html에 대한 URL 패턴 추가
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
