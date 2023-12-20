e5fbdecad7980e0fcb90f80a4cb46e9f5c892be3
hyeon8922 <hyeon8922@naver.com>
2023-12-20 09:39:47 +0900

    hjj

--- big_project/urls.py
+++ big_project/urls.py
@@ -9,13 +9,21 @@ from django.conf.urls.static import static
 def index(request):
     return render(request, 'index.html')
 
+def generic(request):
+    return render(request, 'generic.html')
+ 
+def elements(request):
+    return render(request, 'elements.html')
+
 urlpatterns = [
     path('', index),
     path("admin/", admin.site.urls),
     path('account/', include('account.urls')),
     path('chatbot/', include('chatbot.urls')),
     path('client/', include('client.urls')),
-    path('index.html', index, name='index'),  # /index.html에 대한 URL 패턴 추가
+    path('index.html', index, name='index'),
+    path('generic.html', generic, name='generic'),
+    path('elements.html', elements, name='elements'),
 ]
 
 if settings.DEBUG:
