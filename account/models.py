e0e3214f90b4be4a1e81d35eb1b281227013779f
minsung <sbr04119@naver.com>
2023-12-20 18:00:12 +0900

    fix error

--- account/models.py
+++ account/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 from django.conf import settings
 
-<<<<<<< HEAD
+
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-=======
-# Create your models here.
-
->>>>>>> 6a747eb4b0ee48a1f8f077a6ed90900a6a520ae3
