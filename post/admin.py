a7650e0bab6f0ebdde6b18e246be69c3ac37533a
minsung <sbr04119@naver.com>
2024-01-02 15:54:46 +0900

    change client, post admin

--- post/admin.py
+++ post/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
-# 게시글(Post) Model을 불러옵니다
-
-admin.site.register(Comment)
+    list_display = ('title', 'author', 'created')
+    
+    
+admin.site.register(Post, PostAdmin)
